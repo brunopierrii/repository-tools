22d7313b3c6f77db37189139e5efdbf7996c86a4
Bruno Pierri <pierri95@gmail.com>
2023-10-16 14:10:34 -0300

    code refactoring, and applying clean code, to user data endpoints

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,29 +5,23 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(#[CurrentUser] ?User $user): Response
     {
-        $users = $doctrine->getRepository(User::class)->findAll();
-        if($users){
-            
-            $response = [];
-            foreach ($users as $user) {
+        $response = [
+            'id' => $user->getId(),
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ];
 
-                $response[] = [
-                    'id' => $user->getId(),
-                    'name' => $user->getName(),
-                    'email' => $user->getEmail(),
-                    'password' => $user->getPassword(),
-                    'roles' => $user->getRoles(),
-                ];
-            }
-            return $this->json($response);
-        }
+        return $this->json($response);
     }
 }
